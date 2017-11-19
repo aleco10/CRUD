f5714028911587b4002ca9588ae390f060d9dedf
Jorge Copara <jorgecopara@gmail.com>
2017-11-19 13:20:00 -0500

    nuevo

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <html>
  <head>
   <title>Prueba de PHP</title>
@@ -31,35 +30,4 @@
   </tr>
 </table>
  </body>
-=======
-<html>
- <head>
-  <title>Prueba de PHP</title>
- </head>
- <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
- <form action="accion.php" method="post">
- <p>Su nombre: <input type="text" name="nombre" /></p>
- <p>Su edad: <input type="text" name="edad" /></p>
- <p><input type="submit" /></p>
-</form>
-<table style="width:100%">
-  <tr>
-    <th>Firstname</th>
-    <th>Lastname</th> 
-    <th>Age</th>
-  </tr>
-  <tr>
-    <td>Jill</td>
-    <td>Smith</td> 
-    <td>50</td>
-  </tr>
-  <tr>
-    <td>Eve</td>
-    <td>Jackson</td> 
-    <td>94</td>
-  </tr>
-</table>
- </body>
->>>>>>> cada4af1fbbdf22053d20f860bd0e49e417a0196
 </html>
