76981449003f8e5439875388425257d663983a8c
Jorge Copara <jorgecopara@gmail.com>
2017-11-19 12:57:13 -0500

    bootstrap

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+  <meta charset="utf-8">
+  <meta charset="utf-8" name="viewport" content="width=device-width, user-scalable=no">
+  <<link rel="stylesheet" href="css/bootstrap.min.css">
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+ <form action="accion.php" method="post">
+ <p>Su nombre: <input type="text" name="nombre" /></p>
+ <p>Su edad: <input type="text" name="edad" /></p>
+ <button class="btn btn-primary">enviar</button>
+</form>
+<table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td> 
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>94</td>
+  </tr>
+</table>
+ </body>
+</html>
